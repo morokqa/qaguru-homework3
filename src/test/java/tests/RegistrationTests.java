9996507b7f2ea5ec69316a58d67b9add888264ed
Marina Stepanova <123195046+marinastepanova-qa@users.noreply.github.com>
2025-06-10 13:22:16 +0300

    hw 11 added screenshot attach

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -3,11 +3,9 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.*;
@@ -25,6 +23,12 @@ public class RegistrationTests {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+    }
+
     @Test
     @DisplayName("Проверка полностью заполненной формы")
     @Tag("demoqa")
